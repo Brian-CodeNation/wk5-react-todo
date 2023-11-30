e8506ab2c45bf6d348fd041c131d445048c9f05d
Brian-CodeNation <brian.harkins@wearecodenation.com>
2023-11-30 16:27:29 +0000

    Added code for the todo functions

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-
 import { useState } from 'react';
 import './App.css';
 
 const App = () => {
-  const [inputtext, setinputtext] = useState("");
+  const [inputText, setInputText] = useState("");
   const [items, setItems] = useState([]);
   const [toggle, setToggle] = useState(false);
 
   const AddItem = () => {
     let listCopy = [...items];
-    listCopy.push(inputtext)
+    listCopy.push(inputText)
     setItems(listCopy);
   }
 
@@ -19,43 +18,40 @@ const App = () => {
     setItems(listCopy)
   }
 
-  const crossitem = inputtext => {
+  const crossItem = inputText => {
+    setToggle(!toggle)    
 
-    setToggle(!toggle)
-    let listCopy = [...items];
-
-    if (inputtext.target.parentElement.style.textDecoration === "line-through") {
-      inputtext.target.parentElement.style.setProperty(`text-decoration`, "none");
+    if (inputText.target.parentElement.style.textDecoration === "line-through") {
+      inputText.target.parentElement.style.setProperty(`text-decoration`, "none");
     } else {
-      inputtext.target.parentElement.style.setProperty('text-decoration', 'line-through');
+      inputText.target.parentElement.style.setProperty('text-decoration', 'line-through');
     }
-
   }
 
   return (
-    // Current list for editing
+
     <div className="App">
-      <div>
-        <h1 className='Title'>My ToDo List</h1>
+      <h1>My ToDo List</h1>
+      
+      <div className="addItem">
+        <input className="inputBox" type="text" placeholder='Add to list' value={inputText} onChange={(event) => setInputText(event.target.value)} ></input>
+        <button className="addButton" onClick={AddItem}>Add</button>
       </div>
-      <div className='Addinglist'>
 
-        <input id='box' type="text" placeholder='Add to list' value={inputtext} onChange={(event) => setinputtext(event.target.value)} ></input>
-
-        <button id='Plus' onClick={AddItem}>Add list</button>
-        <div className='texts'>{items.map((item, index) => {
-          return (
-            <div id='colourbox'>
-              <h4 key={index} id="checkedlist"> {item}
-                <button id='ticked' onClick={crossitem}>Done?</button>
-                <button id='Minus' onClick={() => deleteItem(index)}>X</button>
-              </h4>
-            </div>
-          )
-        })}</div>
-
+      <div className="items">
+          {items.map((item, index) => {
+            return (
+              <div className="eachItem">
+                <h4 key={index}>{item}</h4>
+                <button className="button" onClick={crossItem}>Done?</button>
+                <button className="button" onClick={() => deleteItem(index)}>Remove</button>
+              </div>
+            )
+          })}
       </div>
+
     </div>
+    
   )
 
 }
